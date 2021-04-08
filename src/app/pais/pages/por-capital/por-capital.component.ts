943fca7206c8bff3a29c9b5dc47eec1e6936bbbd
leovi martinez <lmartinez@legosoft.com.mx>
2021-04-08 16:11:04 -0500

    actualizacion de codigo

---
+++ src/app/pais/pages/por-capital/por-capital.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
+
+@Component({
+  selector: 'app-por-capital',
+  templateUrl: './por-capital.component.html',
+  styles: [
+  ]
+})
+export class PorCapitalComponent {
+
+  termino: string = '';
+  sinInfo: boolean = false;
+  paises: Country[] = [];
+
+  constructor(private paisService: PaisService) { }
+
+  buscar( termino: string ) {
+    this.sinInfo = false;
+    this.termino = termino;
+    console.log(this.termino);
+    this.paisService.buscarCapital( this.termino )
+    .subscribe( (paises) => {
+      console.log(paises);
+      this.paises = paises;
+    }, (err) => {
+      this.sinInfo = true
+      this.paises = [];
+    } )
+  }
+
+  sugerencias(termino: string) {
+    this.sinInfo = false;
+  }
+
+}
---
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
+
+@Component({
+  selector: 'app-por-region',
+  templateUrl: './por-region.component.html',
+  styles: [`
+    button {
+      margin-right: 5px;
+    }
+  `
+  ]
+})
+export class PorRegionComponent {
+
+  regiones: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania'];
+  regionActiva: string = '';
+  paises: Country[] = [];
+  region: string = '';
+
+  constructor(private paisService: PaisService) { }
+
+  getClaseCSS(region: string) {
+    return (region === this.regionActiva) ? 'btn btn-primary' : 'btn btn-outline-primary';
+  }
+
+  activarRegion(region: string) {
+    if ( region === this.regionActiva ) { return; }
+    this.regionActiva = region;
+    this.region = region;
+    this.paises = [];
+    this.paisService.buscarPorRegion(this.region)
+      .subscribe( (paises) => {
+      this.paises = paises;
+    });
+  }
+
+}
